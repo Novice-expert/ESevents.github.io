0ffa240245e00578a6f7aba4d0a6b4724772034d
Novice-expert <neerubharti2@gmail.com>
2022-05-29 00:57:21 +0530

    Frontend part 1 update

--- components/_navBar.js
+++ components/_navBar.js
@@ -1,26 +1,31 @@
-
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import {Container } from 'react-bootstrap';
-// import { NavDropdown} from 'react-bootstrap';
-//import { COMPONENT } from 'react-bootstrap';
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 
 
-const _navBar =() =>{
+
+
+
+
+const _navbar = () =>{
+
     return (
-<>
-<Navbar bg="light" expand="lg" background-color=" rgb(95,174,31)">
+        <>
+<Navbar bg="light" expand="lg">
   <Container>
     {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#link">Home</Nav.Link>
-        <Nav.Link href="#link">Team</Nav.Link>
-        <Nav.Link href="#link">Events</Nav.Link>
-        <Nav.Link href="#link">Speakers</Nav.Link>
-        <Nav.Link href="#link">Sponsors</Nav.Link>
-        <Nav.Link href="#link">FAQ</Nav.Link>
+      <Nav className="me-auto"> 
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#home">Team</Nav.Link>
+        <Nav.Link href="#home">Events</Nav.Link>
+        <Nav.Link href="#home">Speakers</Nav.Link>
+        <Nav.Link href="#home">Sponsors</Nav.Link>
+        <Nav.Link href="#home">FAQ</Nav.Link>
+
+
         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -28,22 +33,20 @@ const _navBar =() =>{
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown> */}
-        
+
+
+
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
 
-{/* <style>
-.color-nav{
-  background-color:green;
-}
-</style> */}
-</>
+        </>
+
 
 
     );
 
-    }
 
-    export default _navBar;
+}
+export default _navbar;
